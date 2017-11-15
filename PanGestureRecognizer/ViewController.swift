aaaabf245089c8c202519fa4c314e060203bf1d3
Jorge Antonio <jorge_mr@ingenieria.unam.mx>
2017-11-15 17:13:57 -0600

    Rellena la pantalla de vistas de colores aleatoriamente

--- PanGestureRecognizer/ViewController.swift
+++ PanGestureRecognizer/ViewController.swift
@@ -12,9 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let numberOfSquareViews = 15
+        let width = view.frame.width / CGFloat(numberOfSquareViews)
+        let numberOfSquareViewsY = Int(view.frame.height / width)
+        print(numberOfSquareViewsY)
+        for j in 0...numberOfSquareViewsY {
+            for i in 0...numberOfSquareViews {
+                let squareView = UIView()
+                squareView.frame = CGRect(x: CGFloat(i) * width , y: CGFloat(j) * width, width: width, height: width)
+                squareView.backgroundColor = randomColor()
+                view.addSubview(squareView)
+            }
+        }
     }
 
+    func randomColor() -> UIColor {
+        let redColor = CGFloat(arc4random_uniform(100))/100
+        let greenColor = CGFloat(arc4random_uniform(100))/100
+        let blueColor = CGFloat(arc4random_uniform(100))/100
+        return UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
