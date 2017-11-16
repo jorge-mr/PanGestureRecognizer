58222e6e0b49e869489aa98f7712def4606246d9
Jorge MR <jorge_mr@icloud.com>
2017-11-15 20:59:19 -0600

    App terminada

--- PanGestureRecognizer/ViewController.swift
+++ PanGestureRecognizer/ViewController.swift
@@ -9,22 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var numberOfSquareViewsHorizontaly = 25
+    var numberOfSquareViewsVerticaly = Int()
+    var screen = [String:UIView]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let numberOfSquareViews = 15
-        let width = view.frame.width / CGFloat(numberOfSquareViews)
-        let numberOfSquareViewsY = Int(view.frame.height / width)
-        print(numberOfSquareViewsY)
-        for j in 0...numberOfSquareViewsY {
-            for i in 0...numberOfSquareViews {
+        let width = view.frame.width / CGFloat(numberOfSquareViewsHorizontaly)
+        numberOfSquareViewsVerticaly = Int(view.frame.height / width)
+        for j in 0...numberOfSquareViewsVerticaly {
+            for i in 0...numberOfSquareViewsHorizontaly {
                 let squareView = UIView()
                 squareView.frame = CGRect(x: CGFloat(i) * width , y: CGFloat(j) * width, width: width, height: width)
                 squareView.backgroundColor = randomColor()
                 view.addSubview(squareView)
+                screen["\(j),\(i)"] = squareView
             }
         }
+        self.view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(ViewController.expandView)))
     }
 
     func randomColor() -> UIColor {
@@ -33,12 +34,39 @@ class ViewController: UIViewController {
         let blueColor = CGFloat(arc4random_uniform(100))/100
         return UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
     }
+
+    var selectedView : UIView?
     
+    @objc func expandView(gesture: UIPanGestureRecognizer){
+        let width = view.frame.width / CGFloat(numberOfSquareViewsHorizontaly)
+        let position = gesture.location(in: self.view)
+        let i = Int(position.x/width)
+        let j = Int(position.y/width)
+        guard let currentView = screen["\(j),\(i)"] else { return }
+        if currentView != selectedView {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+                self.selectedView?.layer.transform = CATransform3DIdentity
+            }, completion: nil)
+        }
+        
+        self.view.bringSubview(toFront: currentView)
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+                currentView.layer.transform = CATransform3DMakeScale(4, 4, 4)
+        }, completion: nil)
+        
+        if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+                currentView.layer.transform = CATransform3DIdentity
+            }, completion: nil)
+        }
+        selectedView = currentView
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override var prefersStatusBarHidden: Bool {return true}
 
 }
 
